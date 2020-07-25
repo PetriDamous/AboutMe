04cfa35f867331b57b3f362651da5e437331d87c
dwilliams64 <dwilliams_64@yahoo.com>
2020-07-25 11:00:53 -0500

    User can now enter text in text area and it will append to enteris secition of page

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const form = document.getElementById("form");
 const entries = document.getElementById("entries");
+const entryTxtBox = document.getElementsByClassName("entry-textbox");
 
-form.addEventListener("submit", (e) => {
-    console.log(e.target)
+const addEntryToDom = (e) => {    
+    let entryTxt = entryTxtBox[0].value;
+    createEntry(entryTxt);
     e.preventDefault();
-});
+}
+
+const createEntry = (entryTxt) => {
+    let entry = document.createElement("div");
+    entry.classList.add("single-entry");
+    entry.textContent = entryTxt;
+    entries.appendChild(entry);
+}
+
+form.addEventListener("submit", addEntryToDom);
+
